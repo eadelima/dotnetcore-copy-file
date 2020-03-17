ce91d9a6ba96daaa78bf9dc1bd675d66d545137c
Edvan <eadelima@outlook.com>
2020-03-17 12:57:13 -0300

    Implementation

---
+++ CopyFile/CopyFile/Copy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CopyFile
+{
+    public static class Copy
+    {
+        public static void Init(string sourcePath, string targetPath)
+        {
+            DirectoryInfo[] directories = new DirectoryInfo(sourcePath).GetDirectories();
+
+            string[] files = Directory.GetFiles(sourcePath);
+
+            Directory.CreateDirectory(targetPath);
+
+            CopyFileToDest(files, targetPath);
+
+            foreach (var directory in directories)
+            {
+                string dirPath = Path.Combine(targetPath, directory.Name);
+
+                Init(directory.FullName, dirPath);
+            }
+        }
+        public static void CopyFileToDest(string[] files, string targetPath)
+        {
+            string fileName = "";
+            string destiny = "";
+
+            foreach (string file in files)
+            {
+                fileName = Path.GetFileName(file);
+                destiny = Path.Combine(targetPath, fileName);
+                File.Copy(file, destiny, true);
+            }
+        }
+    }
+}
---
+++ CopyFile/CopyFile/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"D:\data\src\genciador-de-arquivos\public";
+            string targetPath = @"D:\data\src\genciador-de-arquivos\private";
+
+            Copy.Init(sourcePath, targetPath);
+        }
+    }
+}
